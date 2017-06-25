c9ff63837b4471568b98be3894d911456927ae2c
martec <mr.martchero@gmail.com>
2017-06-25 18:29:36 +0900

    1.3.5

--- inc/plugins/automention.php
+++ inc/plugins/automention.php
@@ -20,7 +20,7 @@ if(!defined("IN_MYBB"))
 	die("Direct initialization of this file is not allowed.<br /><br />Please make sure IN_MYBB is defined.");
 }
 
-define('AM_PLUGIN_VER', '1.3.4');
+define('AM_PLUGIN_VER', '1.3.5');
 
 function automention_info()
 {
