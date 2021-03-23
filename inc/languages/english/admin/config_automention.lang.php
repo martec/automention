c1aeceebd1140e25b225f45092d6e7ef97079fe5
Laird <lairdshaw77@gmail.com>
2021-03-24 07:58:21 +1000

    Improve the wordings of the settings

--- inc/languages/english/admin/config_automention.lang.php
+++ inc/languages/english/admin/config_automention.lang.php
@@ -1,17 +1,16 @@
 <?php
-$l['automention_sett_desc'] = 'Settings related to the Automention.';
-$l['automention_onoff_title'] = 'Enable or Disable the Automention';
-$l['automention_onoff_desc'] = 'Set to yes if you want to enable the Automention.';
-$l['automention_limitems_title'] = 'Limit number of items to show in popup list';
-$l['automention_limitems_desc'] = 'Set to here limit number of items to show in popup list.';
-$l['automention_maxlenght_title'] = 'Max length of the string';
-$l['automention_maxlenght_desc'] = 'Set to here max length of the string after `at` that would be matched.';
-$l['automention_avatar_title'] = 'Avatar Support';
-$l['automention_avatar_desc'] = 'Set to yes if you do want enable avatar support.';
-$l['automention_space_title'] = 'Space Support';
-$l['automention_space_desc'] = 'Set to yes if you do want enable space support.<br />Ps. This feature can degrade fluidity in typing.';
-$l['automention_fulltext_title'] = 'Full-text search Support';
-$l['automention_fulltext_desc'] = 'Set to yes if you do want enable full-text search Support.';
-$l['automention_threadpart_title'] = 'Thread participants Support';
-$l['automention_threadpart_desc'] = 'Set to yes if you do want enable thread participants Support.';
-?>
+$l['automention_sett_desc'] = 'Settings related to the Automention plugin';
+$l['automention_onoff_title'] = 'Enable automention?';
+$l['automention_onoff_desc'] = 'Set to "Yes" if you want to enable automention; no to disable it';
+$l['automention_limitems_title'] = 'Maximum pop-up items';
+$l['automention_limitems_desc'] = 'Set here the maximum number of items to show in the automention pop-up.';
+$l['automention_maxlength_title'] = 'Maximum mention match length';
+$l['automention_maxlength_desc'] = 'Set here the maximum length of the string after `at` (@) to try to match.';
+$l['automention_avatar_title'] = 'Support avatars?';
+$l['automention_avatar_desc'] = 'Set to "Yes" if you want to enable avatar support in the automention pop-up.';
+$l['automention_space_title'] = 'Support spaces?';
+$l['automention_space_desc'] = 'Set to "Yes" if you want to enable support for spaces in usernames to be auto-completed.<br />N.B. This feature can degrade fluidity in typing.';
+$l['automention_fulltext_title'] = 'Support full-text search?';
+$l['automention_fulltext_desc'] = 'Set to "Yes" if you want to match anywhere inside usernames, rather than from only the beginning of usernames.';
+$l['automention_threadpart_title'] = 'Support thread participant listings?';
+$l['automention_threadpart_desc'] = 'Set to "Yes" if you want the automention pop-up for an empty match (an @ character alone) to list thread participants.';
